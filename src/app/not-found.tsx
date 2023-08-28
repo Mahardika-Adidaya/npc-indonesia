c3317f8100fd9084f0c6401b45bd3bf1dde7ee97
randibprananda <randibprananda@programmer.net>
2023-08-28 10:00:43 +0700

    ✏️ Add page 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+  return (
+    <div className="w-full flex justify-center items-center">
+      <img src="/404.png" />
+    </div>
+  );
+};
+
+export default NotFoundPage;
