06eb6bca12f13421051badd2a5aedcf69f8a0c55
randibprananda <randibprananda@programmer.net>
2023-08-28 15:41:20 +0700

    ✏️ Add responsive detail news

--- src/app/news/[title]/page.tsx
+++ src/app/news/[title]/page.tsx
@@ -7,14 +7,15 @@ import SportCardTwo from '@/components/sport/card-two';
 const DetailNewsPage = () => {
   return (
     <div className="w-full bg-white">
-      <div className="py-[86px] gap-y-[60px] px-[70px] w-full max-w-[1440px] mx-auto flex flex-col">
-        <section className="flex flex-col gap-y-[28px]">
-          <h1 className="font-poppins font-[700] text-[40px]">
+      <div className="py-[29px] xl:py-[86px] gap-y-5 xl:gap-y-[60px] px-2 xl:px-[70px] w-full max-w-[1440px] mx-auto flex flex-col">
+        <section className="flex flex-col gap-y-[12px] xl:gap-y-[28px]">
+          <h1 className="font-poppins font-[700] text-[20px] xl:text-[40px]">
             President Jokowi Honors Paralympic Athletes at State Palace for
             Remarkable Achievements
           </h1>
-          <span className="flex gap-5 items-center text-[20px]">
+          <span className="flex gap-5 items-center text-[12px] xl:text-[20px]">
             <h3 className="font-[400]">9 August 2023</h3>
+            <div className="rounded-full w-[6px] h-[6px] bg-hitam-400 block xl:hidden" />
             <h3 className="font-[500]">General News</h3>
           </span>
           <Image
@@ -22,9 +23,9 @@ const DetailNewsPage = () => {
             alt="news"
             width={1300}
             height={624}
-            className="w-[1300px] h-[624px] object-cover"
+            className="w-auto xl:w-[1300px] h-[165px] xl:h-[624px] object-cover"
           />
-          <p className="font-poppins font-[400] text-justify text-[24px]">
+          <p className="font-poppins font-[400] text-justify text-[12px] xl:text-[24px]">
             In a heartwarming and inspiring ceremony, President Joko Widodo,
             affectionately known as Jokowi, extended his heartfelt
             congratulations and honored the nation&apos;s exceptional Paralympic
@@ -75,21 +76,23 @@ const DetailNewsPage = () => {
         <section>
           <div className="text-hitam-700 w-full justify-between flex items-center">
             <span>
-              <h2 className="font-[700] text-[38px]">Related News</h2>
-              <h3 className="font-[400] text-[16px]">
+              <h2 className="font-[700] text-[20px] xl:text-[38px]">
+                Related News
+              </h2>
+              <h3 className="font-[400] text-[10px] xl:text-[16px]">
                 The most popular news you should know to keep up with the latest
                 news
               </h3>
             </span>
             <Link
               href="#"
-              className="flex items-center gap-x-3 font-[500] text-[20px]"
+              className="items-center gap-x-3 font-[500] text-[20px] hidden xl:flex"
             >
               Show More
               <BsArrowRight />
             </Link>
           </div>
-          <div className="w-full grid grid-cols-3 mt-10">
+          <div className="w-full flex scrollbar-hide overflow-x-scroll gap-x-[21px] mt-5">
             <SportCardTwo link="#" />
             <SportCardTwo link="#" />
             <SportCardTwo link="#" />
