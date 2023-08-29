b3f9fd6ad1027d328182568ea9710db41e3e88c1
randibprananda <randibprananda@programmer.net>
2023-08-29 09:30:56 +0700

    💄 Update mobile responsive error, not found page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,7 +2,7 @@
 
 const Error = () => {
   return (
-    <div className="w-full flex justify-center items-center">
+    <div className="w-full overflow-hidden h-screen flex justify-center items-center">
       <img src="/500.png" />
     </div>
   );
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,9 @@
-const NotFoundPage = () => {
+const NotFound = () => {
   return (
-    <div className="w-full flex justify-center items-center">
+    <div className="w-full overflow-hidden h-screen flex justify-center items-center">
       <img src="/404.png" />
     </div>
   );
 };
 
-export default NotFoundPage;
+export default NotFound;
