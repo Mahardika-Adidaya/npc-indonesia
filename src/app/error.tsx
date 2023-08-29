f9ff93b768ffbafa852aca822d1da88357c78ae4
randibprananda <randibprananda@programmer.net>
2023-08-29 09:26:07 +0700

    💄 Add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const Error = () => {
+  return (
+    <div className="w-full flex justify-center items-center">
+      <img src="/500.png" />
+    </div>
+  );
+};
+
+export default Error;
