60a9c95b68d41411bc88de2905fd49fd769964c1
randibprananda <randibprananda@programmer.net>
2023-08-30 11:11:24 +0700

    💄 Update heading text component

--- src/components/ui/heading-text.tsx
+++ src/components/ui/heading-text.tsx
@@ -8,14 +8,17 @@ interface HeadingTextProps {
 
 const HeadingText: FC<HeadingTextProps> = ({ children }) => {
   return (
-    <h2
-      className={cn(
-        'text-merah-500 underline decoration-biru-600 text-[20px] xl:text-[64px] font-[400] decoration-2 xl:decoration-8 mb-5 xl:mb-[40px]',
-        fontAhrefs400.className
-      )}
-    >
-      {children}
-    </h2>
+    <div className="w-fit flex flex-col mb-5 xl:mb-[40px] relative">
+      <h2
+        className={cn(
+          'text-merah-500 text-[20px] md:text-[38px] xl:text-[64px] font-[400]',
+          fontAhrefs400.className
+        )}
+      >
+        {children}
+      </h2>
+      <div className="bg-biru-600 w-full h-[2px] md:h-[4px] xl:h-[8px] absolute bottom-[4px] xl:bottom-[10px]"></div>
+    </div>
   );
 };
 
