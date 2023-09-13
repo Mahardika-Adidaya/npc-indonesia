9eaaa3f24d83dcfc717c02e3e88e2d145faa669f
randibprananda <randibprananda@programmer.net>
2023-09-13 20:42:42 +0700

    update setup next config images

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '5000',
-        pathname: '/**'
-      }
-    ]
+    domains: [process.env.NEXT_PUBLIC_API_IMAGE]
   }
 };
 
