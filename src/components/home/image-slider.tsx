fe62ecdb28a32f6d6dc9f18c83c010d87b74b8e2
Muhammad Iqbal Ainu Rafie <74098172+ainurafie@users.noreply.github.com>
2023-09-13 13:59:07 +0700

    fixing carousel in homepage

--- src/components/home/image-slider.tsx
+++ src/components/home/image-slider.tsx
@@ -1,13 +1,53 @@
+'use client';
+import React, { useState, useEffect } from 'react';
 import CardImageSlider from '@/components/news/card-image-slider';
 import { Progress } from '@/components/ui/progress';
 import { cn } from '@/lib/utils';
 
-const ImageSliderHome = () => {
+const ImageSliderHome: React.FC = () => {
+  const data = [
+    {
+      title: 'Slide title for 1',
+      imageUrl: '/dummy/banner-one.png'
+    },
+    {
+      title: 'Slide title for 2',
+      imageUrl: '/dummy/news.png'
+    },
+    {
+      title: 'Slide title for 3',
+      imageUrl: '/dummy/banner-one.png'
+    }
+  ];
+
+  const totalSlides = data.length;
+  const [currentSlide, setCurrentSlide] = useState<number>(0);
+  const [progressValue, setProgressValue] = useState<number>(0);
+
+  const handleProgressBarClick = (index: number) => {
+    setCurrentSlide(index);
+    setProgressValue(0);
+  };
+
+  useEffect(() => {
+    const progressInterval = setInterval(() => {
+      setProgressValue(prevValue => {
+        if (prevValue >= 100) {
+          setCurrentSlide(prevSlide => (prevSlide + 1) % totalSlides);
+          return 0;
+        }
+        return prevValue + 1;
+      });
+    }, 50);
+
+    return () => clearInterval(progressInterval);
+  }, [currentSlide]);
+
   return (
     <div className="w-full flex flex-col gap-y-[16px] mx-auto">
       <section
         className="w-full h-[233px] md:h-[422px] xl:h-[598px] bg-cover rounded-[20px] relative overflow-hidden"
-        style={{ backgroundImage: "url('/dummy/banner-one.png')" }}
+        style={{ backgroundImage: `url('${data[currentSlide].imageUrl}')` }}
       >
         <div
           className="w-1/3 absolute left-0 h-full rounded-[20px]"
@@ -20,14 +60,13 @@ const ImageSliderHome = () => {
           className="absolute z-10 bottom-0 w-full h-1/3"
           style={{
             background:
-              ' linear-gradient(360deg, #000 0%, rgba(0, 0, 0, 0.00) 100%)'
+              'linear-gradient(360deg, #000 0%, rgba(0, 0, 0, 0.00) 100%)'
           }}
         />
         <div className="absolute bottom-[24px] z-40 p-[8px] md:p-[28px] xl:p-[52px] lg:py-[30px] text-white">
           <div className="w-full xl:w-[443px] flex flex-col md:gap-3 xl:gap-y-[20px]">
             <h1 className="text-[14px] md:text-[24px] xl:text-[36px] font-[700]">
-              President Jokowi Honors Paralympic Athletes at State Palace for
-              Remarkable Achievements
+              {data[currentSlide].title}
             </h1>
             <div className="text-[8px] xl:text-[16px] flex gap-x-[8px] items-center">
               <h4 className="font-[400] text-white">9 August 2023</h4>
@@ -42,54 +81,29 @@ const ImageSliderHome = () => {
             </div>
           </div>
           <div className="w-full flex mt-[20px] md:mt-[35px] xl:mt-[80px] space-x-3 xl:space-x-[45px]">
-            <div className="w-fit flex flex-col gap-y-[8px]">
-              <Progress
-                className="h-[2px] w-[58px] xl:w-[212px] xl:h-[10px]"
-                value={33}
-              />
-              <h5
-                className={cn(
-                  'text-[14px] font-[400] text-hitam-300 hidden xl:block',
-                  {
-                    active: 'text-white'
-                  }
-                )}
+            {data.map((slide, index) => (
+              <div
+                key={index}
+                className="w-fit flex flex-col gap-y-[8px]"
+                onClick={() => handleProgressBarClick(index)}
               >
-                Day one to crown 26 new world champions
-              </h5>
-            </div>
-            <div className="w-fit flex flex-col gap-y-[8px]">
-              <Progress
-                className="h-[2px] w-[58px] xl:w-[212px] xl:h-[10px]"
-                value={33}
-              />
-              <h5
-                className={cn(
-                  'text-[14px] font-[400] text-hitam-300 hidden xl:block',
-                  {
-                    active: 'text-white'
-                  }
-                )}
-              >
-                Day one to crown 26 new world champions
-              </h5>
-            </div>
-            <div className="w-fit flex flex-col gap-y-[8px]">
-              <Progress
-                className="h-[2px] w-[58px] xl:w-[212px] xl:h-[10px]"
-                value={33}
-              />
-              <h5
-                className={cn(
-                  'text-[14px] font-[400] text-hitam-300 hidden xl:block',
-                  {
-                    active: 'text-white'
-                  }
-                )}
-              >
-                Day one to crown 26 new world champions
-              </h5>
-            </div>
+                <Progress
+                  className="h-[2px] w-[58px] xl:w-[212px] xl:h-[10px]"
+                  value={index === currentSlide ? progressValue : 0}
+                  style={{ color: 'white' }}
+                />
+                <h5
+                  className={cn(
+                    'text-[14px] font-[400] text-hitam-300 hidden xl:block',
+                    {
+                      active: index === currentSlide
+                    }
+                  )}
+                >
+                  {slide.title}
+                </h5>
+              </div>
+            ))}
           </div>
         </div>
       </section>
