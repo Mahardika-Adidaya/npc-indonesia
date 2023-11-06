2359d73d33157fb96091bf17d6d51e1be05ec78f
Jahfal Rizqi <jahfalrizqi14@gmail.com>
2023-11-06 13:36:44 +0700

    FIX NEXT CONFIG

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: process.env.NEXT_PUBLIC_MODE == "dev" ? process.env.NEXT_PUBLIC_API_IMAGE_STAGING : process.env.NEXT_PUBLIC_API_IMAGE_PROD
+    domains: [process.env.NEXT_PUBLIC_API_IMAGE_PROD,process.env.NEXT_PUBLIC_API_IMAGE_STAGING]
   }
 };
 
